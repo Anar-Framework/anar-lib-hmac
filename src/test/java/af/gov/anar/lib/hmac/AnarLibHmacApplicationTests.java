defa2d8a16a7648043f8bef70310210481ec80a8
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-13 15:26:09 +0430

    hmac utility library for anar framework

---
+++ src/test/java/af/gov/anar/lib/hmac/AnarLibHmacApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.hmac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibHmacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
